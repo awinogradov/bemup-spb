41de95696a02fb42269615503f5af9ed8622f57d
Anton Winogradov <winogradovaa@gmail.com>
2014-09-02 00:29:28 +0400

    fix statusbar

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -8,7 +8,6 @@
         { elem: 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1.0' } },
         { elem: 'meta', attrs : { name : 'apple-mobile-web-app-title', content : 'BEMup' } },
         { elem: 'meta', attrs : { name : 'apple-mobile-web-app-capable', content : 'yes' } },
-        { elem: 'meta', attrs : { name : 'apple-mobile-web-app-status-bar-style', content : 'black-translucent' } },
         { elem: 'link', attrs : { href : 'images/apple-touch-icon.png', rel : 'apple-touch-icon' } },
         { elem: 'link', attrs : { href : 'images/apple-touch-icon-120x120.png', rel : 'apple-touch-icon', size : '120x120' } },
     ],
@@ -18,10 +17,7 @@
             elem : 'header',
             tag : 'header',
             content : [
-                {
-                    block : 'city',
-                    content : 'Москва'
-                },
+                { block : 'city', content : 'Москва' },
                 {
                     block : 'title',
                     content : [
